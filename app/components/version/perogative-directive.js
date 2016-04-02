a38dcfbcfa088b78b19b19042b6e2f38a140074f
larrychen <dachao.chen@accenture.com>
2016-04-02 18:18:11 +0800

    load html and json in UT of directive

--- app/components/version/perogative-directive.js
+++ app/components/version/perogative-directive.js
@@ -3,23 +3,26 @@ angular.module('myApp.version.perogative-directive', [])
 
 .directive('myPerogative', [ function() {
 
-	return {
-		restrict : 'EA',
-		// template: '<button class="btn btn-primary" ng-model="perogative"  ng-click="updateButton()" ng-dblclick="dblClickUpdate()">{{perogative}}</button>',
-		templateUrl : 'tpl/perogative.html',
-		scope : true,
-		link : function(scope, element, attrs) {
+    return {
+        restrict : 'EA',
+        // template: '<button class="btn btn-primary" ng-model="perogative"  ng-click="updateButton()" ng-dblclick="dblClickUpdate()">{{perogative}}</button>',
+        templateUrl : 'tpl/perogative.html',
+        scope : true,
+        controller: function ($scope) {
+            console.log('in controller');
+        },
+        link : function(scope, element, attrs) {
 
-			scope.perogative = 'Click Me';
+            scope.perogative = 'Click Me';
 
-			scope.updateButton = function() {
-				scope.perogative = 'My Other Perogative';
-			}
+            scope.updateButton = function() {
+                scope.perogative = 'My Other Perogative';
+            }
 
-			scope.dblClickUpdate = function() {
-				scope.perogative = 'My Other Other Perogative';
-			}
+            scope.dblClickUpdate = function() {
+                scope.perogative = 'My Other Other Perogative';
+            }
 
-		}
-	}
+        }
+    }
 } ])
--- test/unit/components/version/perogative-directive.spec.js
+++ test/unit/components/version/perogative-directive.spec.js
@@ -1,73 +1,73 @@
 describe('myApp.version.perogative-directive', function () {
 
-	
-	describe('perogative directive', function () {
-		var elm, scope, $compile, servedData;
-		
-		function compileElement() {
-			elm = angular.element(elm);
-			$compile(elm)(scope);
-			scope.$digest();
-		}
-		
-		beforeEach(module('myApp.version'));
-		// load html templates that html2js plugin have convert to angular module and don't need to inject any more
-		beforeEach(module('templates'));
-		// load the json file that json2js plugin have convert to angular module and inject the constant
-		beforeEach(module('served/data.json'));
-		
-	  	beforeEach(inject(function (_$rootScope_, _$compile_, _servedData_) {
-	  		debugger
-	  		$compile = _$compile_;
-	  		scope = _$rootScope_;
-	  		servedData = _servedData_;
-	  		
-	  		elm = '<div my-perogative></div>';
-		    
-		    compileElement();
-		}));
-	  	
-	  	it('Test mocked json api & json2js plugin', function () {
-	  		 expect(servedData).toEqual({
-	             prop: 'val'
-	         });
-	  	});
 
-	    describe('when page compiles the perogative directive', function () {
+    describe('perogative directive', function () {
+        var elm, scope, $compile, servedData;
 
-	        it('display directive button on page with interpolated value for button label', function () {
-	            expect(elm.html()).toContain('Click Me');
-	        });
+        function compileElement() {
+            elm = angular.element(elm);
+            $compile(elm)(scope);
+            scope.$digest();
+        }
 
-	        it('should give another value for perogative scope variable', function(){
-	            //Change scope variable to Something completely different
-	            elm.scope().perogative = 'Sweet Chili Sauce';
+        beforeEach(module('myApp.version'));
+        // load html templates that html2js plugin have convert to angular module and don't need to inject any more
+        beforeEach(module('templates'));
+        // load the json file that json2js plugin have convert to angular module and inject the constant
+        beforeEach(module('served/data.json'));
 
-	            //call the updateButton function on isolate scope
-	            elm.scope().updateButton();
+          beforeEach(inject(function (_$rootScope_, _$compile_, _servedData_) {
+              debugger
+              $compile = _$compile_;
+              scope = _$rootScope_;
+              servedData = _servedData_;
 
-	            //digest the $rootScope
-	            scope.$digest()
+              elm = '<div my-perogative></div>';
 
-	            //test to make sure value changed which tests the updateButton Function
-	            expect(elm.scope().perogative).toBe('My Other Perogative');
-	        });
-	        
-	        it('should give another value for perogative scope variable when double clicked', function(){
-	            //Change scope variable to Something completely different
-	            elm.scope().perogative = 'My Other Perogative';
+            compileElement();
+        }));
 
-	            //call the dblClickUpdate function on isolate scope
-	            elm.scope().dblClickUpdate();
+          it('Test mocked json api & json2js plugin', function () {
+               expect(servedData).toEqual({
+                 prop: 'val'
+             });
+          });
 
-	            //digest the $rootScope
-	            scope.$digest()
+        describe('when page compiles the perogative directive', function () {
 
-	            //test to make sure value changed which tests the updateButton Function
-	            expect(elm.scope().perogative).toBe('My Other Other Perogative');
-	        });
+            it('display directive button on page with interpolated value for button label', function () {
+                expect(elm.html()).toContain('Click Me');
+            });
 
-	    });
-	});
-	
+            it('should give another value for perogative scope variable', function(){
+                //Change scope variable to Something completely different
+                elm.scope().perogative = 'Sweet Chili Sauce';
+
+                //call the updateButton function on isolate scope
+                elm.scope().updateButton();
+
+                //digest the $rootScope
+                scope.$digest()
+
+                //test to make sure value changed which tests the updateButton Function
+                expect(elm.scope().perogative).toBe('My Other Perogative');
+            });
+
+            it('should give another value for perogative scope variable when double clicked', function(){
+                //Change scope variable to Something completely different
+                elm.scope().perogative = 'My Other Perogative';
+
+                //call the dblClickUpdate function on isolate scope
+                elm.scope().dblClickUpdate();
+
+                //digest the $rootScope
+                scope.$digest();
+
+                //test to make sure value changed which tests the updateButton Function
+                expect(elm.scope().perogative).toBe('My Other Other Perogative');
+            });
+
+        });
+    });
+
 });
