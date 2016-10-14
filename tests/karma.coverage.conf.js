36718742ab74ce786395eb72961082be649d9c3c
chendachao <chendachao@outlook.com>
2016-10-14 19:09:26 +0800

    safe

--- tests/karma.coverage.conf.js
+++ tests/karma.coverage.conf.js
@@ -8,7 +8,7 @@ module.exports = function(config){
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', 'coverage'],
     
     files : [
       // libraries
