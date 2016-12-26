b489ead2fe88209b13229618b24c3369d0418996
chendachao <chendachao@outlook.com>
2016-12-26 17:57:42 +0800

    add protractor-jasmine2-screenshot-reporter

--- tests/e2e/e2e/protractor.conf.js
+++ tests/e2e/e2e/protractor.conf.js
@@ -1,10 +1,22 @@
 var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+
+var jasmine2HtmlReporter =  new Jasmine2HtmlReporter({
+  savePath: 'report/e2e/protractor-jasmine2-html-reporter/',
+  filePrefix: 'index',
+  screenshotsFolder: 'screenshots'
+});
+
+var htmlScreenshotReporter = new HtmlScreenshotReporter({
+  dest: 'report/e2e/protractor-jasmine2-screenshot-reporter/',
+  filename: 'index.html'
+});
 
 exports.config = {
   allScriptsTimeout: 11000,
 
   specs: [
-    '*.js'
+    '*.e2e.js'
   ],
 
   capabilities: {
@@ -20,12 +32,24 @@ exports.config = {
     showColors: true
   },
   
+  // Setup the report before any tests start
+  beforeLaunch: function() {
+    return new Promise(function(resolve){
+      htmlScreenshotReporter.beforeLaunch(resolve);
+    });
+  },
+  
+  // Assign the test reporter to each running instance
   onPrepare: function () {
-    jasmine.getEnv().addReporter(
-      new Jasmine2HtmlReporter({
-        savePath: 'report/e2e/',
-        screenshotsFolder: 'screenshots'
-      })
-    );
+    jasmine.getEnv().addReporter(jasmine2HtmlReporter);
+    jasmine.getEnv().addReporter(htmlScreenshotReporter);
+  },
+  
+  // Close the report after all tests finish
+  afterLaunch: function(exitCode) {
+    return new Promise(function(resolve){
+      htmlScreenshotReporter.afterLaunch(resolve.bind(this, exitCode));
+    });
   }
+  
 };
